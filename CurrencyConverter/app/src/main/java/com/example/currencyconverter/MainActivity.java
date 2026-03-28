0d7666ac035d559e301aa05d06a9592ee0073500
CoderPrateek971 <prateekgarg0305@gmail.com>
2026-03-28 23:35:39 +0530

    conversion logic added

--- CurrencyConverter/app/src/main/java/com/example/currencyconverter/MainActivity.java
+++ CurrencyConverter/app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.currencyconverter;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner fromCurrency, toCurrency;
+    EditText amount;
+    TextView result;
+    Button converterBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,4 +31,44 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+    private void convertCurrency() {
+
+        if (amount.getText().toString().isEmpty()) {
+            result.setText(getString(R.string.result_format));
+            return;
+        }
+
+        double amt = Double.parseDouble(amount.getText().toString());
+
+        String from = fromCurrency.getSelectedItem().toString();
+        String to = toCurrency.getSelectedItem().toString();
+
+        double rate = getRate(from, to);
+        double converted = amt * rate;
+
+        result.setText(getString(R.string.result_format,converted);
+    }
+    private double getRate(String from, String to) {
+
+        double inr;
+
+        switch (from) {
+            case "USD": inr = 94.86; break;
+            case "EUR": inr = 109.01; break;
+            case "JPY": inr = 0.59; break;
+            default: inr = 1;
+        }
+
+        double target;
+
+        switch (to) {
+            case "USD": target = 1 / 94.86; break;
+            case "EUR": target = 1 / 109.01; break;
+            case "JPY": target = 1 / 0.59; break;
+            default: target = 1;
+        }
+
+        return inr * target;
+    }
+
 }
