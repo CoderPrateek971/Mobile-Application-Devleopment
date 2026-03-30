9e91f798258444bc524f68d367c51b72f346368d
CoderPrateek971 <prateekgarg0305@gmail.com>
2026-03-30 20:20:22 +0530

    logic added

--- Sensor Data/app/src/main/java/com/example/sensordata/MainActivity.java
+++ Sensor Data/app/src/main/java/com/example/sensordata/MainActivity.java
@@ -1,24 +1,58 @@
 package com.example.sensordata;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 
+import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    private Sensor accelerometer, lightSensor, proximitySensor;
+
+    private TextView accelText, lightText, proximityText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        accelText = findViewById(R.id.accelText);
+        lightText = findViewById(R.id.lightText);
+        proximityText = findViewById(R.id.proximityText);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
+        if (accelerometer == null) {
+            accelText.setText(getString(R.string.not_available));
+        }
+        if (lightSensor == null) {
+            lightText.setText(getString(R.string.not_available));
+        }
+        if (proximitySensor == null) {
+            proximityText.setText(getString(R.string.not_available));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
     }
 }
